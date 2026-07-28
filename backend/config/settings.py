52896cf052214dc2affd9551ffa16a836f6bbde5
Praveen Kumar <vrpraveenkumar2003@gmail.com>
2026-07-28 16:03:37 +0530

    Updated settings for Render deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1%z614p=x+a6^+oaa)g!vl+n+n=$_0mjir1r%x3+1@o_i&+-b2'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-1%z614p=x+a6^+oaa)g!vl+n+n=$_0mjir1r%x3+1@o_i&+-b2"
+)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -128,3 +131,18 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
+# Email Configuration
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "vrpraveenkumar2003@gmail.com"
+
+EMAIL_HOST_PASSWORD = "xhqe ihvp zvwh dthx"
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- backend/contact/views.py
+++ backend/contact/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from django.core.mail import send_mail
+from django.conf import settings
+
 from .models import ContactMessage
 from .serializers import ContactSerializer
 
@@ -6,5 +9,31 @@ from .serializers import ContactSerializer
 class ContactCreateView(generics.CreateAPIView):
 
     queryset = ContactMessage.objects.all()
-
     serializer_class = ContactSerializer
+
+    def perform_create(self, serializer):
+
+        contact = serializer.save()
+
+        send_mail(
+            subject=f"New Contact Message from {contact.name}",
+
+            message=f"""
+Name: {contact.name}
+
+Email: {contact.email}
+
+Phone: {contact.phone}
+
+Message:
+{contact.message}
+""",
+
+            from_email=settings.EMAIL_HOST_USER,
+
+            recipient_list=[
+                "vrpraveenkumar2003@gmail.com"
+            ],
+
+            fail_silently=False,
+        )
